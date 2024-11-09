53b9916024868514c2ad65444350c212908b4885
Extrovert <2038290448@qq.com>
2024-11-09 15:55:47 +0800

    modify report_event api

--- src/router/report_event.ts
+++ src/router/report_event.ts
@@ -2,38 +2,38 @@ import type { AxiosError, AxiosResponse } from 'axios'
 import axios from 'axios'
 
 interface ReportEventParams {
-    data: {
-        eventName: string;
-        tags: string;
-    }[];
+  data: {
+    eventName: string;
+    tags: string;
+  }[];
 }
 
 function reportEvent(params: ReportEventParams): Promise<AxiosResponse> {
   const { data } = params;
 
-//   // 创建一个 Blob 对象来保存数据
-//   const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+  //   // 创建一个 Blob 对象来保存数据
+  //   const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
 
-//   // 创建一个下载链接并点击以下载文件
-//   const url = URL.createObjectURL(blob);
-//   const link = document.createElement('a');
-//   link.href = url;
-//   link.download = 'report-events.json'; // 指定文件名
-//   document.body.appendChild(link);
-//   link.click();
-//   document.body.removeChild(link);
-//   URL.revokeObjectURL(url);
+  //   // 创建一个下载链接并点击以下载文件
+  //   const url = URL.createObjectURL(blob);
+  //   const link = document.createElement('a');
+  //   link.href = url;
+  //   link.download = 'report-events.json'; // 指定文件名
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  //   URL.revokeObjectURL(url);
 
   const config = {
     method: 'post',
     url: `https://api.xhpolaris.com/platform/report_event`,
     headers: {
       'Content-Type': 'application/json',
-      'Authentication': localStorage.getItem('user-token') || '',
+      'Authorization': localStorage.getItem('user-token') || '',
     },
     data: {
-        'data':data 
-    } 
+      'data': data
+    }
   }
 
   return axios(config)
